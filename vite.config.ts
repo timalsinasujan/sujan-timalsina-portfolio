068bba56fb20cb8a57839bf5562d3082a78d9aa2
Sujan Timalsina <smarttechzvlogs@gmail.com>
2025-11-29 17:10:33 +0545

    fix: update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // 🌟 CORRECTED BASE PATH (changed 's' to 'a' in 'timalsina')
-  base: "/sujan-timalaina-portfolio/", 
   plugins: [react()],
+  base: '/sujan-timalsina-portfolio/',
 })
